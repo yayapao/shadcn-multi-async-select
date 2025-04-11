165266dbb7c69080367d1223993170cd23b0585d
yanghao2 <yanghao2@huya.com>
2025-04-11 18:01:01 +0800

    feat: add city api

---
+++ src/app/api/city/route.ts
@@ -0,0 +1,28 @@
+import path from "path";
+import fs from "node:fs";
+
+// get rand user object
+export async function GET(request: Request) {
+  // get query params
+  const { searchParams } = new URL(request.url);
+  const keyword = searchParams.get("keyword");
+  try {
+    // 构建文件路径
+    const filePath = path.resolve("src/app/api/city/data.json");
+    // 读取文件
+    const data = fs.readFileSync(filePath, "utf-8");
+    // 解析文件
+    const res = JSON.parse(data);
+    // 如果有关键字，则过滤
+    if (keyword) {
+      const cities = res.data.filter((item: { name: string }) =>
+        item.name.toLowerCase().includes(keyword.toLowerCase())
+      );
+      return Response.json(cities);
+    }
+    // 返回用户
+    return Response.json(res);
+  } catch (error) {
+    return Response.json({ message: "Server error" + error });
+  }
+}
