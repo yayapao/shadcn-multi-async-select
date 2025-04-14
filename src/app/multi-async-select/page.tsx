30a048c077d0c94d436b39f8bf37bfa08ca1833b
yanghao2 <yanghao2@huya.com>
2025-04-14 16:45:22 +0800

    fix: vercel building

--- src/app/multi-async-select/page.tsx
+++ src/app/multi-async-select/page.tsx
@@ -1,16 +1,14 @@
-import MultiAsyncSelectClient from "./client";
+// import MultiAsyncSelectClient from "./client";
 
 export default async function MultiAsyncSelectPage() {
-  const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/city`);
-  const cities = await data.json();
+  // const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/city`);
+  // const cities = await data.json();
   return (
     <div className="flex flex-col gap-4 p-4">
       <header className="flex items-center justify-between">
         <h1 className="text-2xl font-bold">Multi Async Select</h1>
       </header>
-      <main>
-        <MultiAsyncSelectClient options={cities.data} />
-      </main>
+      <main>{/* <MultiAsyncSelectClient options={cities.data} /> */}</main>
     </div>
   );
 }
