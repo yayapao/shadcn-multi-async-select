93a478c49ba46443ebabab2e91b757a3187b4836
yayapao <luckyyoung666@gmail.com>
2025-04-15 11:28:29 +0800

    feat: add tabs for usage

--- src/app/multi-async-select/client.tsx
+++ src/app/multi-async-select/client.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { MultiAsyncSelect } from "@/components/open-source/multi-async-select";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useMutation } from "@tanstack/react-query";
 import { useDebouncedCallback } from "use-debounce";
 
@@ -35,28 +36,47 @@ export default function MultiAsyncSelectClient({ options }: Props) {
 
   return (
     <div className="flex flex-col gap-4 items-center justify-center">
-      <div>
-        <h5 className="text-sm font-medium mb-2">Async-Multi</h5>
-        <MultiAsyncSelect
-          loading={isPending}
-          error={error}
-          options={data?.data || []}
-          onValueChange={(value) => console.log(value)}
-          className="w-[540px]"
-          onSearch={handleSearch}
-          async
-        />
-      </div>
-      <div className="mt-4">
-        <h5 className="text-sm font-medium mb-2">
-          Also support multiple select only
-        </h5>
-        <MultiAsyncSelect
-          options={options}
-          onValueChange={(value) => console.log(value)}
-          className="w-[540px]"
-        />
-      </div>
+      <Tabs defaultValue="async" className="w-[540px]">
+        <TabsList className="grid w-full grid-cols-3">
+          <TabsTrigger value="async">Async</TabsTrigger>
+          <TabsTrigger value="sync">Sync</TabsTrigger>
+          <TabsTrigger value="form">Form</TabsTrigger>
+        </TabsList>
+        <TabsContent value="async">
+          <div>
+            <h5 className="text-sm font-medium mb-2">
+              Async data-loading multi-select, support search & debounce, built
+              with shadcn/ui popover, command, and more.
+            </h5>
+            <MultiAsyncSelect
+              loading={isPending}
+              error={error}
+              options={data?.data || []}
+              onValueChange={(value) => console.log(value)}
+              className="w-[540px]"
+              onSearch={handleSearch}
+              async
+            />
+          </div>
+        </TabsContent>
+        <TabsContent value="sync">
+          <div>
+            <h5 className="text-sm font-medium mb-2">
+              Also support multiple select only
+            </h5>
+            <MultiAsyncSelect
+              options={options}
+              onValueChange={(value) => console.log(value)}
+              className="w-[540px]"
+            />
+          </div>
+        </TabsContent>
+        <TabsContent value="form">
+          <div>
+            <h5 className="text-sm font-medium mb-2">Form</h5>
+          </div>
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
