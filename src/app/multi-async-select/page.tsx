8321228b8610ca8f4880c199a5ba1a2eb7c0e96c
yayapao <luckyyoung666@gmail.com>
2025-04-15 11:02:41 +0800

    feat: add line-shadown-text

--- src/app/multi-async-select/page.tsx
+++ src/app/multi-async-select/page.tsx
@@ -1,4 +1,4 @@
-import { LineShadowText } from "@/components/yaui/text/magic-line-shadow-text";
+import { LineShadowText } from "@/components/magicui/line-shadow-text";
 import MultiAsyncSelectClient from "./client";
 
 export default async function MultiAsyncSelectPage() {
@@ -7,9 +7,9 @@ export default async function MultiAsyncSelectPage() {
   return (
     <div className="w-full max-w-6xl mx-auto flex flex-col gap-4 mt-16 md:mt-24 justify-center items-center">
       <div className="flex items-center justify-between">
-        <h1 className="text-balance text-5xl font-semibold leading-none tracking-tighter sm:text-6xl md:text-7xl lg:text-8xl">
+        <h1 className="text-balance text-6xl font-semibold leading-none tracking-tighter">
           Multi Async
-          <LineShadowText className="italic">Select</LineShadowText>
+          <LineShadowText className="italic pl-2">Select</LineShadowText>
         </h1>
       </div>
       <div className="w-full">
