538f1f8249788474129498fc59cfd15695728166
To My <60420319+MyFRA@users.noreply.github.com>
2025-05-20 22:18:49 +0700

    fix add deps in optionsRefs

--- src/components/open-source/multi-async-select.tsx
+++ src/components/open-source/multi-async-select.tsx
@@ -207,7 +207,7 @@ export const MultiAsyncSelect = React.forwardRef<HTMLButtonElement, Props>(
           ...temp2,
         };
       }
-    }, [options, options, selectedValues]);
+    }, [async, options, selectedValues]);
 
     return (
       <Popover
